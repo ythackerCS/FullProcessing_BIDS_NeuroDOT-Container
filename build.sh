26e244fad8092e5592a42f0ef0a6633a8018ba8b
ythackerCS <46691555+ythackerCS@users.noreply.github.com>
2024-03-07 16:16:47 -0600

    updated names for build and command

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp Dockerfile.base Dockerfile && \
 ./command2label.py ./xnat/command.json >> Dockerfile && \
-docker build --no-cache -t xnat/preprocessingneurodot_bids:latest .
-docker tag xnat/preprocessingneurodot_bids:latest registry.nrg.wustl.edu/docker/nrg-repo/yash/preprocessingneurodot_bids:latest
+docker build --no-cache -t xnat/fullprocessingneurodot_bids:latest .
+docker tag xnat/fullprocessingneurodot_bids:latest registry.nrg.wustl.edu/docker/nrg-repo/yash/fullprocessingneurodot_bids:latest
 rm Dockerfile
